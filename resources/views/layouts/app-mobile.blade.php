7a6c88dce86f1020840f7a918e2be4974c1d2d2d
jonhrogielmades <jonhrogielmades@gmal.com>
2026-06-27 22:15:38 +0800

    Fix broken base64 profile photos in mobile drawer

--- resources/views/layouts/app-mobile.blade.php
+++ resources/views/layouts/app-mobile.blade.php
@@ -461,7 +461,7 @@
                @if(Auth::check() && Auth::user()->profile_photo_path)
                   @php
                       $photoPath = Auth::user()->profile_photo_path;
-                      $photoUrl = str_starts_with($photoPath, 'http') ? $photoPath : asset('storage/' . $photoPath);
+                      $photoUrl = (str_starts_with($photoPath, 'http') || str_starts_with($photoPath, 'data:')) ? $photoPath : asset('storage/' . $photoPath);
                   @endphp
                   <img src="{{ $photoUrl }}" alt="Avatar" style="width:100%;height:100%;object-fit:cover;">
                @else
