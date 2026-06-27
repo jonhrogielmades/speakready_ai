fcd630d0824a08f53a0413da55c1143b696edae7
jonhrogielmades <jonhrogielmades@gmal.com>
2026-06-27 09:25:34 +0800

    Revert IPv4 fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,15 +22,5 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
-
-        // Force Socialite to use IPv4 to prevent 504 Gateway Timeouts on Render
-        if (class_exists(\Laravel\Socialite\Facades\Socialite::class)) {
-            \Laravel\Socialite\Facades\Socialite::extend('google', function ($app) {
-                $config = $app['config']['services.google'];
-                $provider = \Laravel\Socialite\Facades\Socialite::buildProvider(\Laravel\Socialite\Two\GoogleProvider::class, $config);
-                $provider->setHttpClient(new \GuzzleHttp\Client(['curl' => [CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4]]));
-                return $provider;
-            });
-        }
     }
 }
