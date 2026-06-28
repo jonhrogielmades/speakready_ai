01c96b72c086ee600b83bcef0e3e0ce99a4c0b07
jonhrogielmades <jonhrogielmades@gmal.com>
2026-06-28 19:41:05 +0800

    Fix Category CRUD by adding type to fillable

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['title', 'description', 'icon', 'status', 'is_featured', 'sort_order'];
+    protected $fillable = ['title', 'type', 'description', 'icon', 'status', 'is_featured', 'sort_order'];
 
     public function questions()
     {
