12e9841c31393f48fcb4e024205e26e9555061a3
jonhrogielmades <jonhrogielmades@gmal.com>
2026-07-31 13:19:57 -0900

    Use white PWA icon background

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'speakready-pwa-v12';
+const CACHE_NAME = 'speakready-pwa-v13';
 const STATIC_ASSET_PATTERN = /\.(?:css|js|png|jpg|jpeg|gif|svg|webp|ico|woff2?|ttf|eot)$/i;
 
 function isSameOrigin(requestUrl) {
