91c451ae025ba6c9533d82a8b442508bcd7a7b48
jonhrogielmades <jonhrogielmades@gmal.com>
2026-08-26 22:44:14 +0800

    Update mobile account and coach UI

--- resources/views/mobile/user/account.blade.php
+++ resources/views/mobile/user/account.blade.php
@@ -3,7 +3,7 @@
 
 @push('styles')
 <link rel="stylesheet" href="{{ asset('css/mobile/user/account.css?v=2') }}" data-page-style="user-account">
-<link rel="stylesheet" href="{{ asset('css/mobile/user/account-2.css?v=2') }}" data-page-style="user-account-2">
+<link rel="stylesheet" href="{{ asset('css/mobile/user/account-2.css?v=3') }}" data-page-style="user-account-2">
 @endpush
 
 @section('content')
