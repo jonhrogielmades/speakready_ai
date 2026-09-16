7fb210189809b8518f7ed56099208a052a45ecb3
jonhrogielmades <jonhrogielmades@gmal.com>
2026-09-16 15:55:30 +0800

    Move modules modal close button to top right

--- resources/views/desktop/user/modules/index.blade.php
+++ resources/views/desktop/user/modules/index.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Interview Modules')
 
 @push('styles')
-<link rel="stylesheet" href="{{ asset('css/desktop/user/modules/index.css?v=5') }}" data-page-style="user-modules-index">
+<link rel="stylesheet" href="{{ asset('css/desktop/user/modules/index.css?v=6') }}" data-page-style="user-modules-index">
 @endpush
 
 @section('content')
@@ -196,7 +196,7 @@
  <input type="hidden" name="category" value="{{ $currentCategory }}">
  <input type="hidden" name="search" value="{{ $currentSearch }}">
  <div class="modal-header">
- <button type="button" class="btn-close module-position-close-left" data-bs-dismiss="modal" aria-label="Close"></button>
+ <button type="button" class="btn-close module-position-close-right" data-bs-dismiss="modal" aria-label="Close"></button>
  <div class="module-position-heading">
  <div class="module-position-kicker">Interview Modules</div>
  <h5 class="modal-title" id="modulePositionModalTitle">What position are you applying for?</h5>
